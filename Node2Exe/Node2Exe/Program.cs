ec88d5561c6b255567d749ad0c179d22bccc58dc
guillaumeguerin <guerin_guillaume@hotmail.fr>
2016-08-19 18:22:32 +0200

    Added port check functions

--- Node2Exe/Node2Exe/Program.cs
+++ Node2Exe/Node2Exe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,44 @@ namespace Node2Exe
 {
     class Program
     {
+        public static bool checkPort(int port)
+        {
+            bool isAvailable = true;
+
+            // Evaluate current system tcp connections. This is the same information provided
+            // by the netstat command line application, just in .Net strongly-typed object
+            // form.  We will look through the list, and if our port we would like to use
+            // in our TcpClient is occupied, we will set isAvailable to false.
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+            {
+                if (tcpi.LocalEndPoint.Port == port)
+                {
+                    isAvailable = false;
+                    break;
+                }
+            }
+
+            // At this point, if isAvailable is true, we can proceed accordingly.
+            return isAvailable;
+        }
+
+        public static int getUnusedPort()
+        {
+            int port = 8080; //<--- Default port
+
+            while (checkPort(port)) {
+                port++;
+            }
+
+            return port;
+        }
+
         static void Main(string[] args)
         {
+            int port = getUnusedPort();
         }
     }
 }
